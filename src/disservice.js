e333f00b819510f12d4272fae302eba5ae04b7a6
Ben Glassman <ben@vtdesignworks.com>
2014-05-02 10:58:11 -0400

    Clean up to use prototypical inheritance for multiple instances.

--- src/disservice.js
+++ src/disservice.js
@@ -18,138 +18,139 @@
   }
 })("Disservice", function () {
 
-    var d = function(serviceProviders, params) {
+    // Check if a variable is a function, from underscore.js
+    var isFunction = function(arg) {
+        return !!(arg && arg.constructor && arg.call && arg.apply)
+    };
 
-        // Disservice
-        var _ds = this;
+    /**
+     * Function for extracting argument names from a function
+     */
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG_SPLIT = /,/;
+    var FN_ARG = /^\s*(_?)(.+?)\1\s*$/;
+    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    var annotate = function(fn) {
+        var $inject,
+        fnText,
+        argDecl,
+        last;
+
+        if (typeof fn == 'function') {
+            if (!($inject = fn.$inject)) {
+                $inject = [];
+                fnText = fn.toString().replace(STRIP_COMMENTS, '');
+                argDecl = fnText.match(FN_ARGS);
+                argDecl[1].split(FN_ARG_SPLIT).forEach(function(arg){
+                    arg.replace(FN_ARG, function(all, underscore, name){
+                        $inject.push(name);
+                    });
+                });
+                fn.$inject = $inject;
+            }
+        } else if (Array.isArray(fn)) {
+            last = fn.length - 1;
+            $inject = fn.slice(0, last);
+        }
+        return $inject;
+    };
+
+    var Disservice = function(serviceProviders, params) {
 
         // Hash of callables which provider an instance of a service
-        var _serviceProviders = serviceProviders || {};
+        this.serviceProviders = serviceProviders || {};
 
         // Hash of service instances
-        var _services = {};
-        var _aliases = {};
+        this.services = {};
+        this.aliases = {};
 
         // Hash of parameters
-        var _params = params || {};
+        this.params = params || {};
 
+    };
+
+    Disservice.prototype = {
         // Determine if a provider for a service is available
-        var _providerExists = function(name) {
-            return name in _serviceProviders;
-        };
+        providerExists: function(name) {
+            return name in this.serviceProviders;
+        },
 
         // Determine if service instance exists
-        var _serviceExists = function(name) {
-            return name in _services || name in _aliases;
-        };
-
-        // Get a service by name or alias
-        var _get = function(name) {
-            name = name in _aliases ? _aliases[name] : name;
-            return name in _services ? _services[name] : null;
-        };
+        serviceExists: function(name) {
+            return name in this.services;
+        },
 
         // Create a new service from a provider
-        var _createService = function(name) {
-            if (!_providerExists(name)) throw new Error('Provider not found for service: ' + name);
-            return _services[name] = _serviceProviders[name].apply(_ds);
-        };
-
-        // Check if a variable is a function, from underscore.js
-        var _isFunction = function(arg) {
-            return !!(arg && arg.constructor && arg.call && arg.apply)
-        };
-
-        /**
-         * Function for extracting argument names from a function
-         */
-        var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-        var FN_ARG_SPLIT = /,/;
-        var FN_ARG = /^\s*(_?)(.+?)\1\s*$/;
-        var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-        var annotate = function(fn) {
-            var $inject,
-            fnText,
-            argDecl,
-            last;
-
-            if (typeof fn == 'function') {
-                if (!($inject = fn.$inject)) {
-                    $inject = [];
-                    fnText = fn.toString().replace(STRIP_COMMENTS, '');
-                    argDecl = fnText.match(FN_ARGS);
-                    argDecl[1].split(FN_ARG_SPLIT).forEach(function(arg){
-                        arg.replace(FN_ARG, function(all, underscore, name){
-                            $inject.push(name);
-                        });
-                    });
-                    fn.$inject = $inject;
-                }
-            } else if (isArray(fn)) {
-                last = fn.length - 1;
-                $inject = fn.slice(0, last);
-            }
-            return $inject;
-        }
+        createService: function(name) {
+            if (!this.providerExists(name)) throw new Error('Provider not found for service: ' + name);
+            return this.services[name] = this.serviceProviders[name].apply(this);
+        },
 
         // Add a new service provider
-        this.add = function(name, service) {
-            if (_providerExists(name)) throw new Error('Service already exists with name: ' + name);
-            _serviceProviders[name] = service;
+        add: function(name, service) {
+            if (this.providerExists(name)) throw new Error('Service already exists with name: ' + name);
+            this.serviceProviders[name] = service;
             return this;
-        };
+        },
 
         // Create an alias for a service
-        this.alias = function(alias, name) {
-            return _aliases[alias] = name;
-        };
+        alias: function(alias, name) {
+            return this.aliases[alias] = name;
+        },
+
+        // Resolve a service name, checking for aliases
+        resolveName: function(name) {
+            return name in this.aliases ? this.aliases[name] : name;
+        },
 
         // Check if a service exists
-        this.has = function(name) {
-            return _serviceExists(name) || _providerExists(name);
-        };
+        has: function(name) {
+            name = this.resolveName(name)
+            return this.serviceExists(name) || this.providerExists(name);
+        },
 
         // Get a service
-        this.get = function(name) {
-            return _serviceExists(name) ? _get(name) : _createService(name);
-        };
+        get: function(name) {
+            name = this.resolveName(name)
+            return this.serviceExists(name) ? this.services[name] : this.createService(name);
+        },
 
         // Set a parameter
-        this.setParameter = function(key, value) {
-            _params[key] = value;
+        setParameter: function(key, value) {
+            this.params[key] = value;
             return this;
-        };
+        },
 
         // Get a parameter
-        this.getParameter = function(key) {
+        getParameter: function(key) {
             if (!this.hasParameter(key)) throw new Error('Invalid parameter ' + key);
 
             // If the parameter is a function, use it to lazy load it to a scalar value
-            if (_isFunction(_params[key])) {
-                this.setParameter(key, _params[key].apply(this));
+            if (isFunction(this.params[key])) {
+                this.setParameter(key, this.params[key].apply(this));
             }
 
-            return _params[key];
-        };
+            return this.params[key];
+        },
 
         // Check if a service exists
-        this.hasParameter = function(name) {
-            return name in _params;
-        };
+        hasParameter: function(name) {
+            return name in this.params;
+        },
 
         // Inject a function with dependencies
-        this.getParameter = function(key) {
-            if (!(key in _params)) throw new Error('Invalid parameter ' + key);
+        getParameter: function(key) {
+            if (!(key in this.params)) throw new Error('Invalid parameter ' + key);
 
             // If the parameter is a function, use it to lazy load it to a scalar value
-            if (_isFunction(_params[key])) {
-                this.setParameter(key, _params[key].apply(this));
+            if (isFunction(this.params[key])) {
+                this.setParameter(key, this.params[key].apply(this));
             }
 
-            return _params[key];
-        };
+            return this.params[key];
+        },
 
-        this.inject = function(fn, defaults) {
+        inject: function(fn, defaults) {
             var args = [],
                 diArgs = annotate(fn),
                 defaults = defaults || {},
@@ -167,9 +168,9 @@
                 }
             }
             return fn.apply(fn, args);
-        };
+        }
     };
 
-    return d;
+    return Disservice;
 
 });
